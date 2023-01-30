8831d82306d632f3ca924cd495df43fed38ed170
Cameron Swiggs <swiggy_13@hotmail.com>
2023-01-30 15:17:50 +1030

    Added Also Github to Sentence Popup

--- src/main/java/com/example/demo2/HelloController.java
+++ src/main/java/com/example/demo2/HelloController.java
@@ -8,7 +8,14 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
+    private Label Twice;
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application! Also Github");
+        welcomeText.setText("Welcome to JavaFX Application! Attempting to Learn");
+    }
+    @FXML
+    protected void onSignInButtonClick() {
+        Twice.setText("Sign In Successful");
     }
 }
