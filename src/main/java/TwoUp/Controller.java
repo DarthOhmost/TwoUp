840b2caea5a94eda74f10b565b53b4140ae84265
Cameron Swiggs <swiggy_13@hotmail.com>
2023-02-02 15:11:14 +1030

    Added Also Github to Sentence Popup

--- src/main/java/TwoUp/Controller.java
+++ src/main/java/TwoUp/Controller.java
@@ -2,7 +2,6 @@ package TwoUp;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
